a8e42dfce9800fe8475c4ae549b43ae36382a0ac
Darci Peoples <darcipeoples@Darcis-MacBook-Pro.local>
2020-12-01 14:30:32 -0600

    Description line up

--- my-app/src/components/request.js
+++ my-app/src/components/request.js
@@ -39,7 +39,7 @@ class Request extends Component {
         return (
             <div className="Request">
                 {!this.state.profile_info || !this.state.icebreaker_chat ? null : 
-                    <div>
+                    <div className='content-container'>
                         <PCard key={this.state.profile_info.uid} info={this.state.profile_info} />
                         <h2><a className='profile-username'>{this.state.icebreaker_chat.uid == this.state.uid1 ? 'Me': this.state.profile_info.username}</a>: {this.state.icebreaker_chat.text}</h2>
                         <Post data={this.state.icebreaker_chat.icebreaker} />
