24efcffe5cfbd087f8936aff5bc1f45744a72f69
Daniela Delgado Ramos <hinohoshi04@gmail.com>
2020-12-07 20:10:51 -0600

    last typo

--- my-app/src/components/signIn.js
+++ my-app/src/components/signIn.js
@@ -6,7 +6,7 @@ class SignIn extends Component {
         return  (
             <div className='access-card'>
                 <h1>Enter</h1>
-                <p>Enter to grok.date usign you Google account, and enjoy the new experience </p>
+                <p>Enter to grok.date using your Google account, and enjoy the new experience</p>
                 <button onClick= {(event) => this.props.login()}>Enter with Google</button>
             </div>
         );
