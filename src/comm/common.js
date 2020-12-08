1be686228a7f99ddd7519386e9414fa85e312ff8
Darci Peoples <darcipeoples@Darcis-MacBook-Pro.local>
2020-12-07 22:23:49 -0600

    cpanel uses prod website

--- src/comm/common.js
+++ src/comm/common.js
@@ -1,11 +1,11 @@
 export const REDDIT = {
-  APP_ID: 'q5rIR3lqrwMirg',
-  APP_SECRET: 'r3ClSlbfmhiI2hYF05GAAqN8CU0jsg',   // TODO darci: should store more securely
-  APP_REDIRECT: 'http://localhost:3000/',
-  APP_NAME: 'grok.date',
+  APP_ID: 'UYQ-F-mjnHCifw',
+  APP_SECRET: 'mJZaWUpA2HKX9cNCw426Tj7QiaNe1Q',   // TODO darci: should store more securely
+  APP_REDIRECT: 'https://grokdating.web.illinois.edu/',
+  APP_NAME: 'grok.date.prod',
 
   SCOPE: 'mysubreddits',
   STATE: 'reddit-oauth2',                         // NOTE darci: maybe should be random, could use redirect URI
   
-  TERMINAL_URI: "http://localhost:3000/",          // Where user will end when all is done
+  TERMINAL_URI: "https://grokdating.web.illinois.edu/",          // Where user will end when all is done
 }
